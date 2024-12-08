b0c5addb0ab3621fd1a41bdeb9304e39beec82ee
thirdone <ethanamt2001@gmail.com>
2024-12-08 17:54:23 +0000

    Testing

--- Resources/WSLEDStripModes/ColourCycleMode.py
+++ Resources/WSLEDStripModes/ColourCycleMode.py
@@ -1,22 +1,21 @@
 from Resources.BlankMode import *
 from Settings.Setting import *
 from rpi_ws281x import Color
-import time
 
-#Set the strip to a solid colour. Not fully working.
-class SetMode(Mode):
+import time
+import random
+
+class ColourCycleMode(Mode):
 
     def __init__(self):
-        self._name = "Set"
+        self._name = "ColourCycle"
         self._fixtureType = "WSLEDStrip"
         self.settings = [
-            Setting("Red", "Sets the Red levels.", 255, int),
-            Setting("Green", "Sets the Green levels.", 0, int),
-            Setting("Blue", "Sets the Blue levels.", 0, int),
+            Setting("ColourDelay", "Colour cycle delay.", random.randrange(0.0, 0.9), float),
             Setting("Brightness", "Sets the Brightness levels.", 255, int)
         ]
 
-    def update(self, fixture):
+    def update(self, fixture):#Not done yet TODO
         for i in range(fixture.getStrip().numPixels()):
             fixture.setPixelColor(i, self.settings[0].getValue(), self.settings[1].getValue(), self.settings[2].getValue(), 0)
             fixture.getStrip().setBrightness(self.settings[3].getValue())
